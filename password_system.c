3bdf9a1f068497e50261aa54783185feb555eaf3
ttyn4519 <37261985+ttyn4519@users.noreply.github.com>
2019-05-21 19:24:50 +0900

    Update password_system.c

--- password_system.c
+++ password_system.c
@@ -3,12 +3,16 @@ int a;
 int b=####; //####には好きな数字を指定,setting your own password to ####
 
 int main(){
+  //パスワードを入力する,write password 
   printf("パスワードを入力してください");
   scanf("%d",&a);
+  //パスワードの正誤判定,check password
+  //正しい数字が入るまで以下の文を繰り返す,Repeat the following statement until you get the correct number
   while(a!=b){
     printf("正しいパスワードを入力してください");
     scanf("%d",&a);
   }
+  //成功した場合に表示する,Display on success
   printf("施錠しました\n");
   return 0;
 }
