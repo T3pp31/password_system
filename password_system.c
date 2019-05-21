d2674f6e1574f6779361f904f154a4e7e21d486b
unknown <ttyn4519@outlook.jp>
2019-05-21 13:35:54 +0900

    add new file

---
+++ password_system.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int a;
+int b=1887; //好きな数字を指定,setting your own password
+
+int main(){
+  printf("パスワードを入力してください");
+  scanf("%d",&a);
+  while(a!=b){
+    printf("正しいパスワードを入力してください");
+    scanf("%d",&a);
+  }
+  printf("施錠しました\n");
+  return 0;
+}
